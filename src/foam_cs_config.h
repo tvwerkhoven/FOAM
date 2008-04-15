d7b745c1afd3f2e18d6dc5b7f9d800f452465ce4
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2008-04-15 13:12:31 +0000

    Update config file, will be deprecated!

--- src/foam_cs_config.h
+++ src/foam_cs_config.h
@@ -7,23 +7,25 @@
 
 	Hardware-dependent variables can be defined here to be used in
 	te rest of @name.
+ 
+	THIS FILE IS DEPRECATED. CONFIGURATION MOVED TO PRIMEMODULE HEADER FILE
 */
 
 // INCLUDE AUTOCONF //
 /********************/
 
-#include "config.h"
+
 
 // USER CONFIG BEGIN //
 /*********************/
-#ifndef FOAM_CS_CONFIG
-#define FOAM_CS_CONFIG
-
-// General configuration
-#define FILENAMELEN 64				//!< maximum length for logfile names (no need to touch)
-#define COMMANDLEN 1024				//!< maximum length for commands we read over the socket (no need to touch)
-
-#define MAX_CLIENTS 8				//!< maximum number of clients that can connect
-#define MAX_THREADS 4				//!< number of threads besides the main thread that can be created (unused atm)
-
-#endif /* FOAM_CS_CONFIG */
+//#ifndef FOAM_CS_CONFIG
+//#define FOAM_CS_CONFIG
+//
+//// General configuration
+//#define FILENAMELEN 64				//!< maximum length for logfile names (no need to touch)
+//#define COMMANDLEN 1024				//!< maximum length for commands we read over the socket (no need to touch)
+//
+//#define MAX_CLIENTS 8				//!< maximum number of clients that can connect
+//#define MAX_THREADS 4				//!< number of threads besides the main thread that can be created (unused atm)
+//
+//#endif /* FOAM_CS_CONFIG */
