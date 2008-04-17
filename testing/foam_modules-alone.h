b3a5515699537be1c1d65c3a07d811174f790394
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2008-04-17 13:29:33 +0000

    added some thing necessary for compiling modules standalone

---
+++ testing/foam_modules-alone.h
@@ -0,0 +1,55 @@
+// Some definities necessary to run modules on their own
+
+#define FILENAMELEN 64
+#define COMMANDLEN 1024
+
+#define MAX_CLIENTS 8
+#define MAX_THREADS 4
+#define MAX_FILTERS 8
+
+typedef enum { // calmode_t
+	CAL_PINHOLE,	
+	CAL_INFL,		
+	CAL_LINTEST	
+} calmode_t;
+
+
+typedef enum { // axes_t
+	WFC_TT=0,		
+	WFC_DM=1		
+} wfctype_t;
+
+typedef enum { //fwheel_t
+	FILT_PINHOLE,	
+	FILT_OPEN,		
+	FILT_CLOSED		
+} fwheel_t;
+
+#include "foam_cs_library.h"		
+/*
+void logErr(const char *msg, ...) {
+	va_list ap;
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	fprintf(stderr, "\n");
+}
+void logWarn(const char *msg, ...) {
+	va_list ap;
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	fprintf(stderr, "\n");
+}
+void logInfo(int flg, const char *msg, ...) {
+	va_list ap;
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	fprintf(stderr, "\n");
+}
+void logDebug(int flg, const char *msg, ...) {
+	va_list ap;
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	fprintf(stderr, "\n");
+}
+
+*/
