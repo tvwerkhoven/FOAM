02ec1f279a6da77b9544dbb4cfec0530d7ed3cfb
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2008-07-23 16:11:02 +0000

    update version script

--- version.sh
+++ version.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ "`svn info | grep ^Revision`" ] ; then
-	echo "hi"
+## If SVN gives a decent value back (i.e. non-null), use that
+	echo "r"`svn info | grep ^Revision | cut -f 2 -d' '`
+else
+## Otherwise use a manual version number
+	echo "r550"
 fi
-#echo "r"`svn info | grep ^Revision | cut -f 2 -d' '`
