922afd308fa2377c20a31cf14707b6a8dc693d73
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2009-10-29 14:26:39 +0100

    Moving and renaming files

---
+++ lib/config.h
@@ -0,0 +1,26 @@
+/* lib/config.h.  Generated from config.h.in by configure.  */
+/* lib/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "foam"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "t.i.m.vanwerkhoven@xs4all.nl"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "FOAM"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "FOAM 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "foam"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
