92ba0b54de1ba34dae0137a3651d23d1c720d93c
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-01-04 18:46:41 +0100

    Expanded protocol test.

--- testing/protocol-test.cc
+++ testing/protocol-test.cc
@@ -8,18 +8,31 @@
 using namespace std;
 typedef Protocol::Server::Connection Connection;
 
-static Protocol::Server *protocol = 0;
-
 static void on_message(Connection *connection, std::string line);
 static void on_connect(Connection *connection, bool status);
 
 int main() {
-	protocol = new Protocol::Server("1234");
+	string msg;
+	
+	Protocol::Server serv("1234");
 
-	protocol->slot_message = sigc::ptr_fun(on_message);
-	protocol->slot_connected = sigc::ptr_fun(on_connect);
-	protocol->listen();
+	serv.slot_message = sigc::ptr_fun(on_message);
+	serv.slot_connected = sigc::ptr_fun(on_connect);
+	serv.listen();
+//	sleep(1);
+	
+	Protocol::Client client("127.0.0.1","1234","");
+	client.connect();
+	sleep(1);
+	
+	msg = "word1 :word2 word3";
+	fprintf(stderr, "client.write(\"%s\");\n", msg.c_str());
+	client.write(msg);
 
+	msg = "word1 word2 word3";
+	fprintf(stderr, "client.write(\"%s\");\n", msg.c_str());
+	client.write(msg);
+	
 	while (1)
 		sleep(1);
 
@@ -27,11 +40,14 @@ int main() {
 }
 
 static void on_connect(Connection *connection, bool status) {
-	fprintf(stderr, "on_connected: %d\n", status);
+	fprintf(stderr, "serv:on_connected: %d\n", status);
 }
 
 static void on_message(Connection *connection, string line) {
-	fprintf(stderr, "on_message: %s\n", line.c_str());
+	fprintf(stderr, "serv:on_message: %s\n", line.c_str());
+	string word;
+	while ((word = popword(line)).length())
+		fprintf(stderr, "serv:on_message: %s\n", word.c_str());
 }
 
 
