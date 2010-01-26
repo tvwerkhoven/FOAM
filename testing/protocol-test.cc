153e8e3f91582fef95e75c5247b24784c76c3960
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-01-26 10:41:09 +0100

    Updated protocol-test to include prefixes.

--- testing/protocol-test.cc
+++ testing/protocol-test.cc
@@ -11,30 +11,39 @@ typedef Protocol::Server::Connection Connection;
 static void on_message(Connection *connection, std::string line);
 static void on_connect(Connection *connection, bool status);
 
+static void on_client_msg(std::string line);
+
 int main() {
 	string msg;
 	
-	Protocol::Server serv("1234");
+	Protocol::Server serv1("1234", "SYS");
+//	Protocol::Server serv2("1234", "WFS1");
 
-	serv.slot_message = sigc::ptr_fun(on_message);
-	serv.slot_connected = sigc::ptr_fun(on_connect);
-	serv.listen();
-//	sleep(1);
+	serv1.slot_message = sigc::ptr_fun(on_message);
+	serv1.slot_connected = sigc::ptr_fun(on_connect);
+	serv1.listen();
+
+//	serv2.slot_message = sigc::ptr_fun(on_message);
+//	serv2.slot_connected = sigc::ptr_fun(on_connect);
+//	serv2.listen();
+	//	sleep(1);
 	
-	Protocol::Client client("127.0.0.1","1234","");
+	Protocol::Client client("127.0.0.1","1234");
 	client.connect();
+	client.slot_message = sigc::ptr_fun(on_client_msg);
 	sleep(1);
 	
-	msg = "word1 :word2 word3";
+	msg = "SYS hello world";
 	fprintf(stderr, "client.write(\"%s\");\n", msg.c_str());
 	client.write(msg);
 
-	msg = "word1 word2 word3";
+	msg = "WFS1 hello sky!";
 	fprintf(stderr, "client.write(\"%s\");\n", msg.c_str());
 	client.write(msg);
 	
-	while (1)
-		sleep(1);
+	//while (1)
+	
+	sleep(2);
 
 	return 0;
 }
@@ -44,10 +53,15 @@ static void on_connect(Connection *connection, bool status) {
 }
 
 static void on_message(Connection *connection, string line) {
-	fprintf(stderr, "serv:on_message: %s\n", line.c_str());
+	fprintf(stderr, "%s:on_message: %s\n", connection->server->name.c_str(), line.c_str());
 	string word;
 	while ((word = popword(line)).length())
 		fprintf(stderr, "serv:on_message: %s\n", word.c_str());
+	
+	fprintf(stderr, "writing\n");
+	connection->write("OK, got it");
 }
 
-
+static void on_client_msg(std::string line) {
+	fprintf(stderr, "cli:on_client_msg: %s\n", line.c_str());
+}
