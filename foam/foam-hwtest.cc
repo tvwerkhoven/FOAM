b754ea89b2d990b8a7e0d4a176372216511bf488
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-09-17 18:11:04 +0200

    Added foam-hwtest target, based on simstatic.

---
+++ foam/foam-hwtest.cc
@@ -0,0 +1,191 @@
+/*
+ foam-hwtest.cc -- hardware test module
+ Copyright (C) 2010 Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
+ 
+ This file is part of FOAM.
+ 
+ FOAM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ FOAM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*! 
+ @file foam-hwtest.c
+ @author Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl)
+ @brief Hardware testing mode, nothing fancy, just a scrapbook of hardware implementations.
+*/
+
+#include <iostream>
+#include <string>
+
+#include "foam.h"
+#include "types.h"
+#include "io.h"
+#include "devices.h"
+#include "camera.h"
+#include "fw1394cam.h"
+
+#include "foam-hwtest.h"
+
+// Global device list for easier access
+FW1394Camera *testcam;
+
+int FOAM_hwtest::load_modules() {
+	io.msg(IO_DEB2, "FOAM_hwtest::load_modules()");
+	io.msg(IO_INFO, "This is FOAM::hwtest.");
+		
+	// Add ImgCam device
+	testcam = new FW1394Camera(io, ptc, "1394testcam", ptc->listenport, ptc->conffile);
+	devices->add((Device *) testcam);
+//	imgcamb = new ImgCamera(io, "imgcamB", ptc->listenport, ptc->cfg);
+//	devices->add((Device *) imgcamb);
+//	imgcamc = new ImgCamera(io, "imgcamC", ptc->listenport, ptc->cfg);
+//	devices->add((Device *) imgcamc);
+	
+	return 0;
+}
+
+// OPEN LOOP ROUTINES //
+/*********************/
+
+int FOAM_hwtest::open_init() {
+	io.msg(IO_DEB2, "FOAM_hwtest::open_init()");
+	
+	((FW1394Camera*) devices->get("1394testcam"))->set_mode(Camera::RUNNING);
+	
+	return 0;
+}
+
+int FOAM_hwtest::open_loop() {
+	io.msg(IO_DEB2, "FOAM_hwtest::open_loop()");
+	static FW1394Camera *tmpcam = ((FW1394Camera*) devices->get("1394testcam"));
+	
+	usleep(1000000);
+	Camera::frame_t *frame = tmpcam->get_last_frame();
+	
+	return 0;
+}
+
+int FOAM_hwtest::open_finish() {
+	io.msg(IO_DEB2, "FOAM_hwtest::open_finish()");
+	
+	((FW1394Camera*) devices->get("dummycam"))->set_mode(Camera::OFF);
+
+	return 0;
+}
+
+// CLOSED LOOP ROUTINES //
+/************************/
+
+int FOAM_hwtest::closed_init() {
+	io.msg(IO_DEB2, "FOAM_hwtest::closed_init()");
+	
+	// Run open-loop init first
+	open_init();
+	
+	return 0;
+}
+
+int FOAM_hwtest::closed_loop() {
+	io.msg(IO_DEB2, "FOAM_hwtest::closed_loop()");
+
+	usleep(10);
+	return 0;
+}
+
+int FOAM_hwtest::closed_finish() {
+	io.msg(IO_DEB2, "FOAM_hwtest::closed_finish()");
+	
+	// Run open-loop finish first
+	open_finish();
+
+	return 0;
+}
+
+// MISC ROUTINES //
+/*****************/
+
+int FOAM_hwtest::calib() {
+	io.msg(IO_DEB2, "FOAM_hwtest::calib()=%s", ptc->calib.c_str());
+
+	if (ptc->calib == "INFLUENCE") {
+		io.msg(IO_DEB2, "FOAM_hwtest::calib INFLUENCE");
+		usleep((useconds_t) 1.0 * 1000000);
+		return 0;
+	}
+	else
+		return -1;
+
+	return 0;
+}
+
+void FOAM_hwtest::on_message(Connection *connection, std::string line) {
+	io.msg(IO_DEB2, "FOAM_hwtest::on_message(line=%s)", line.c_str());
+	netio.ok = true;
+	
+	// First let the parent process this
+	FOAM::on_message(connection, line);
+	
+	string cmd = popword(line);
+	
+	if (cmd == "help") {
+		string topic = popword(line);
+		if (topic.size() == 0) {
+			connection->write(\
+												":==== simstat help ==========================\n"
+												":calib <mode>:           Calibrate AO system.");
+		}
+		else if (topic == "calib") {
+			connection->write(\
+												":calib <mode>:           Calibrate AO system.\n"
+												":  mode=influence:       Measure wfs-wfc influence.\n"
+												":  mode=subapsel:        Select subapertures.");
+		}
+		else if (!netio.ok) {
+			connection->write("err cmd help :topic unkown");
+		}
+	}
+	else if (cmd == "get") {
+		string what = popword(line);
+		if (what == "calib") {
+			connection->write("ok var calib 1 influence");
+		}
+		else if (!netio.ok) {
+			connection->write("err get var :var unkown");
+		}
+	}
+	else if (cmd == "calib") {
+		string calmode = popword(line);
+		connection->write("ok cmd calib");
+		ptc->calib = calmode;
+		ptc->mode = AO_MODE_CAL;
+		pthread_cond_signal(&mode_cond); // signal a change to the main thread
+	}
+	else if (!netio.ok) {
+		connection->write("err cmd :cmd unkown");
+	}
+}
+
+int main(int argc, char *argv[]) {
+	FOAM_hwtest foam(argc, argv);
+	
+	if (foam.has_error())
+		return foam.io.msg(IO_INFO, "Initialisation error.");
+	
+	if (foam.init())
+		return foam.io.msg(IO_ERR, "Configuration error.");
+		
+	foam.io.msg(IO_INFO, "Running hwtest mode");
+	
+	foam.listen();
+	
+	return 0;
+}
---
+++ foam/foam-hwtest.h
@@ -0,0 +1,64 @@
+/*
+ foam-hwtest.h -- hardware testing mode
+ Copyright (C) 2008--2010 Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
+ 
+ This file is part of FOAM.
+ 
+ FOAM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ FOAM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*! 
+	@file foam-hwtest.h
+	@author Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl)
+	@date 2008-04-18 12:55
+
+*/
+
+#ifndef HAVE_FOAM_SIMSTATIC_H
+#define HAVE_FOAM_SIMSTATIC_H
+
+#define FOAM_CONFIG_PRE "foam-simstat"
+
+#include "config.h"
+
+// LIBRARIES //
+/*************/
+
+#include "foam.h"
+#include "types.h"
+#include "io.h"
+
+/*!
+ @brief FOAM hardware test implementation
+*/
+class FOAM_hwtest : public FOAM {
+public:
+	FOAM_hwtest(int argc, char *argv[]): FOAM(argc, argv) { io.msg(IO_DEB2, "FOAM_hwtest::FOAM_hwtest()"); } 
+	virtual ~FOAM_hwtest() { io.msg(IO_DEB2, "FOAM_hwtest::~FOAM_hwtest()"); } 
+	
+	virtual int load_modules();
+	virtual void on_message(Connection *connection, std::string line);
+	
+	virtual int closed_init();
+	virtual int closed_loop();
+	virtual int closed_finish();
+	
+	virtual int open_init();
+	virtual int open_loop();
+	virtual int open_finish();
+	
+	virtual int calib();
+};
+
+#endif // HAVE_FOAM_SIMSTATIC_H
+
