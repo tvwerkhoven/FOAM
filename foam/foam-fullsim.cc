67ea519ee6a18af57687eacf40e842eccb4a94c6
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-09-29 10:20:58 +0200

    Added full simulation mode program.

---
+++ foam/foam-fullsim.cc
@@ -0,0 +1,187 @@
+/*
+ foam-full.cc -- full simulation module
+ Copyright (C) 2010 Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
+ 
+ This file is part of FOAM.
+ 
+ FOAM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ FOAM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*! 
+ @file foam-fullsim.c
+ @author Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl)
+ @brief This is a full simulation mode
+ 
+ This is a full simulation mode, where the complete system from atmosphere to 
+ CCD is taken into account.
+ */
+
+#include <iostream>
+#include <string>
+
+#include "foam.h"
+#include "types.h"
+#include "io.h"
+#include "simulcam.h"
+
+#include "foam-fullsim.h"
+
+// Global device list for easier access
+SimulCam *simcam;
+
+int FOAM_FullSim::load_modules() {
+	io.msg(IO_DEB2, "FOAM_FullSim::load_modules()");
+	io.msg(IO_INFO, "This is the full simulation mode, enjoy.");
+		
+	// Add ImgCam device
+	simcam = new SimulCam(io, ptc, "simcam", ptc->listenport, ptc->conffile);
+	devices->add((Device *) simcam);
+
+	return 0;
+}
+
+// OPEN LOOP ROUTINES //
+/*********************/
+
+int FOAM_FullSim::open_init() {
+	io.msg(IO_DEB2, "FOAM_FullSim::open_init()");
+	
+	((SimulCam*) devices->get("simcam"))->set_mode(Camera::RUNNING);
+	
+	return 0;
+}
+
+int FOAM_FullSim::open_loop() {
+	io.msg(IO_DEB2, "FOAM_FullSim::open_loop()");
+	static SimulCam *tmpcam = ((SimulCam*) devices->get("simcam"));
+	
+	usleep(1000000);
+	Camera::frame_t *frame = tmpcam->get_last_frame();
+	
+	return 0;
+}
+
+int FOAM_FullSim::open_finish() {
+	io.msg(IO_DEB2, "FOAM_FullSim::open_finish()");
+	
+	((SimulCam*) devices->get("simcam"))->set_mode(Camera::OFF);
+
+	return 0;
+}
+
+// CLOSED LOOP ROUTINES //
+/************************/
+
+int FOAM_FullSim::closed_init() {
+	io.msg(IO_DEB2, "FOAM_FullSim::closed_init()");
+	
+	// Run open-loop init first
+	open_init();
+	
+	return 0;
+}
+
+int FOAM_FullSim::closed_loop() {
+	io.msg(IO_DEB2, "FOAM_FullSim::closed_loop()");
+
+	usleep(10);
+	return 0;
+}
+
+int FOAM_FullSim::closed_finish() {
+	io.msg(IO_DEB2, "FOAM_FullSim::closed_finish()");
+	
+	// Run open-loop finish first
+	open_finish();
+
+	return 0;
+}
+
+// MISC ROUTINES //
+/*****************/
+
+int FOAM_FullSim::calib() {
+	io.msg(IO_DEB2, "FOAM_FullSim::calib()=%s", ptc->calib.c_str());
+
+	if (ptc->calib == "INFLUENCE") {
+		io.msg(IO_DEB2, "FOAM_FullSim::calib INFLUENCE");
+		usleep((useconds_t) 1.0 * 1000000);
+		return 0;
+	}
+	else
+		return -1;
+
+	return 0;
+}
+
+void FOAM_FullSim::on_message(Connection *connection, std::string line) {
+	io.msg(IO_DEB2, "FOAM_FullSim::on_message(line=%s)", line.c_str());
+	netio.ok = true;
+	
+	// First let the parent process this
+	FOAM::on_message(connection, line);
+	
+	string cmd = popword(line);
+	
+	if (cmd == "help") {
+		string topic = popword(line);
+		if (topic.size() == 0) {
+			connection->write(\
+												":==== full sim help =========================\n"
+												":calib <mode>:           Calibrate AO system.");
+		}
+		else if (topic == "calib") {
+			connection->write(\
+												":calib <mode>:           Calibrate AO system.\n"
+												":  mode=influence:       Measure wfs-wfc influence.");
+		}
+		else if (!netio.ok) {
+			connection->write("err cmd help :topic unkown");
+		}
+	}
+	else if (cmd == "get") {
+		string what = popword(line);
+		if (what == "calib") {
+			connection->write("ok var calib 1 influence");
+		}
+		else if (!netio.ok) {
+			connection->write("err get var :var unkown");
+		}
+	}
+	else if (cmd == "calib") {
+		string calmode = popword(line);
+		connection->write("ok cmd calib");
+		ptc->calib = calmode;
+		ptc->mode = AO_MODE_CAL;
+		pthread_cond_signal(&mode_cond); // signal a change to the main thread
+	}
+	else if (!netio.ok) {
+		connection->write("err cmd :cmd unkown");
+	}
+}
+
+int main(int argc, char *argv[]) {
+	FOAM_FullSim foam(argc, argv);
+	
+	if (foam.has_error())
+		return foam.io.msg(IO_INFO, "Initialisation error.");
+	
+	if (foam.init())
+		return foam.io.msg(IO_ERR, "Configuration error.");
+		
+	foam.io.msg(IO_INFO, "Running full simulation mode");
+	
+	foam.listen();
+	
+	return 0;
+}
---
+++ foam/foam-fullsim.h
@@ -0,0 +1,67 @@
+/*
+ foam-full.h -- static simulation module header file
+ Copyright (C) 2008--2010 Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
+ 
+ This file is part of FOAM.
+ 
+ FOAM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ FOAM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*! 
+	@file foam-simstatic.h
+	@author Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl)
+	@date 2008-04-18 12:55
+
+*/
+
+#ifndef HAVE_FOAM_FULLSIM_H
+#define HAVE_FOAM_FULLSIM_H
+
+#define FOAM_CONFIG_PRE "foam-fullsim"
+
+
+// LIBRARIES //
+/*************/
+
+#include "autoconfig.h"
+#include "foam.h"
+#include "types.h"
+#include "io.h"
+
+/*!
+ @brief FOAM full simulation implementation
+ 
+ This FOAM implementation provides a complete end-to-end simulation of the 
+ atmosphere, the telescope, the wavefront correctors and -sensors.
+ */
+class FOAM_FullSim : public FOAM {
+public:
+	FOAM_FullSim(int argc, char *argv[]): FOAM(argc, argv) { io.msg(IO_DEB2, "FOAM_FullSim::FOAM_FullSim()"); } 
+	virtual ~FOAM_FullSim() { io.msg(IO_DEB2, "FOAM_FullSim::~FOAM_FullSim()"); } 
+	
+	virtual int load_modules();
+	virtual void on_message(Connection *connection, std::string line);
+	
+	virtual int closed_init();
+	virtual int closed_loop();
+	virtual int closed_finish();
+	
+	virtual int open_init();
+	virtual int open_loop();
+	virtual int open_finish();
+	
+	virtual int calib();
+};
+
+#endif // HAVE_FOAM_FULLSIM_H
+
