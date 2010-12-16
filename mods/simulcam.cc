7e97c017d7b11e9ca7b9f149c87d041fbdf9a6ba
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-12-16 11:46:55 +0100

    Simulcam WIP, now detects subimages/subapertures.

--- mods/simulcam.cc
+++ mods/simulcam.cc
@@ -21,6 +21,7 @@
 #include <unistd.h>
 #include <gsl/gsl_matrix.h>
 #include <math.h>
+#include <fftw3.h>
 
 #include "io.h"
 #include "config.h"
@@ -64,11 +65,9 @@ void SimulCam::gen_telapt() {
 	io.msg(IO_XNFO, "SimulCam::gen_telapt(): init");
 	
 	if (!telapt) {							// Doesn't exist, callocate (all zeros is goed)
-		io.msg(IO_DEB2, "SimulCam::gen_telapt(): calloc");
 		telapt = gsl_matrix_calloc(res.x, res.y);
 	}
 	else if (telapt->size1 != res.x || telapt->size2 != res.y) { // Wrong size, re-allocate
-		io.msg(IO_DEB2, "SimulCam::gen_telapt(): re-calloc");
 		gsl_matrix_free(telapt);
 		telapt = gsl_matrix_calloc(res.x, res.y);
 	}
@@ -89,14 +88,6 @@ void SimulCam::gen_telapt() {
 			}
 		}
 	}
-	
-//	for (size_t i=0; i<telapt->size1; i++)
-//		for (size_t j=0; j<telapt->size2; j++)
-//			sum += gsl_matrix_get(telapt, i, j);
-	
-	io.msg(IO_XNFO, "SimulCam::gen_telapt(): done: minradsq=%g, range=%g--%g, sum=%g", 
-				minradsq, gsl_matrix_min(telapt), gsl_matrix_max(telapt), sum);
-
 }
 
 gsl_matrix *SimulCam::simul_telescope(gsl_matrix *im_in) {
@@ -110,6 +101,11 @@ uint8_t *SimulCam::simul_wfs(gsl_matrix *wave_in) {
 	if (!shwfs)
 		io.msg(IO_ERR | IO_FATAL, "SimulCam::simul_wfs(): cannot simulate wavefront without Shwfs reference.");
 	
+	if (shwfs->mla.nsi <= 0) {
+		io.msg(IO_WARN, "SimulCam::simul_wfs(): no microlenses defined?.");
+		return NULL;
+	}
+	
 	io.msg(IO_DEB2, "SimulCam::simul_wfs()");
 	//! @todo Given a wavefront, image it through a system and return the resulting intensity pattern (i.e. an image).
 	double min=0, max=0, fac;
@@ -117,7 +113,48 @@ uint8_t *SimulCam::simul_wfs(gsl_matrix *wave_in) {
 	fac = 255.0/(max-min);
 	
 	// Apply fourier transform to subimages here
-	//!< @todo convert this
+	coord_t sapos = shwfs->mla.ml[0].pos;
+	coord_t sasize = shwfs->mla.ml[0].size;
+	fftw_plan shplan = NULL;
+	gsl_vector *workspace = gsl_vector_calloc(sasize.x * sasize.y * 4);
+	//fftw_complex
+	
+	for (int n=0; n<shwfs->mla.nsi; n++) {
+		sapos = shwfs->mla.ml[n].pos;
+		sasize = shwfs->mla.ml[n].size;
+		io.msg(IO_DEB2, "SimulCam::simul_wfs() FFT @ %d: (%d,%d)", n, sapos.x, sapos.y);
+		
+		gsl_matrix_view subap = gsl_matrix_submatrix(wave_in, sapos.x, sapos.y, sasize.x, sasize.y);
+		gsl_matrix *subapm = &(subap.matrix);
+		
+		gsl_matrix_scale (subapm, 2.0);
+		
+		for (int i=0; i<sasize.x/2; i++)
+			gsl_matrix_set(subapm, i, 0, 0);
+
+		for (int i=sasize.y*0.25; i<sasize.y*0.75; i++)
+			gsl_matrix_set(subapm, 0, i, 0);
+
+		// Re-alloc data if necessary (should be sasize, but this can vary per subap)
+		if (workspace->size != sasize.x * sasize.y * 4) {
+			gsl_vector_free(workspace);
+			workspace = gsl_vector_calloc(sasize.x * sasize.y * 4);
+		}
+
+		// Copy data to linear array for FFT
+//		for (size_t i=0; i<sasize.x; i++)
+//			for (size_t j=0; j<sasize.y; j++)
+//				gsl_vector_set(workspace,  gsl_matrix_get(subap, i, j);
+
+
+		// Set up FFT plan
+//		if (!shplan)
+//			shplan = fftw_plan_dft_r2c_2d(sasize.x*2, sasize.y*2,
+//																		workspace->data, fftw_complex *out,
+//																		FFTW_MEASURE);
+	}
+		
+		
 	
 	// we take the subaperture, which is shsize.x * .y big, and put it in a larger matrix
 	/*
