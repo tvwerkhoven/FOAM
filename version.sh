f6cb7bfe52413fbce4469e02dc4d9161d9887e70
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2010-12-24 11:51:45 +0100

    Included version strings in executables.

--- version.sh
+++ version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Automatic git revision inclusion, base on
+# <http://groups.google.com/group/memcached/browse_thread/thread/b02e992ede0f0e89>
 VER=`git describe HEAD 2>/dev/null`
 if [ "$VER" ] ; then
 	echo "m4_define([GIT_REVISION], [`echo $VER | tr -d '\n'`])" > version.m4
