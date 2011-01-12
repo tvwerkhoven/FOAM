9fab0f9551cfaa0edcbd2ba5a7ff1faae4ae7f44
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-01-12 12:19:53 +0100

    Updated Zernike:: testing

--- testing/zern-test.cc
+++ testing/zern-test.cc
@@ -21,7 +21,9 @@
 
 #include <gsl/gsl_matrix.h>
 #include <math.h>
+
 #include "imgdata.h"
+#include "format.h"
 
 #include "zernike.h"
 
@@ -40,7 +42,6 @@ int main() {
 	gsl_matrix *rho = zerntest.get_rho();
 	
 	printf("zern-test.cc:: phi size1=%zu, size2=%zu, tda=%zu\n", phi->size1, phi->size2, phi->tda);
-
 	
 	printf("zern-test.cc:: phi (%d, %d) = %g\n", size/2, size/2, gsl_matrix_get(phi, size/2, size/2));
 	printf("zern-test.cc:: phi (0, 0) = %g\n", gsl_matrix_get(phi, 0, 0));
@@ -50,8 +51,8 @@ int main() {
 	printf("zern-test.cc:: rho (0, 0) = %g\n", gsl_matrix_get(rho, 0, 0));
 	printf("zern-test.cc:: rho (%d,%d) = %g\n", size/10, 9*size/10, gsl_matrix_get(rho, size/10, 9*size/10));
 	
-	ImgData phidat(io, (gsl_matrix *) phi, true);
-	phidat.writedata("./zern-test_phidata.fits", ImgData::FITS, true);
+	ImgData phidat(io, (gsl_matrix *) phi);
+	phidat.writedata("./zern-test_phidata.fits", ImgData::FITS);
 	
 	printf("zern-test.cc:: imgdata.phi (%d, %d) = %g\n", size/2, size/2, phidat.getpixel(size/2, size/2));
 	printf("zern-test.cc:: imgdata.phi (%d, %d) = %g\n", 0, 0, phidat.getpixel(0, 0));
@@ -59,8 +60,54 @@ int main() {
 	printf("zern-test.cc:: imgdata.phi (%d, %d) = %g\n", 9*size/10, size/10, phidat.getpixel(9*size/10, size/10));
 	printf("zern-test.cc:: imgdata.phi dt == FLOAT64: %d\n", phidat.getdtype() == FLOAT64);
 	
-	ImgData rhodat(io, (gsl_matrix *) rho, true);
-	rhodat.writedata("./zern-test_rhodata.fits", ImgData::FITS, true);
+	ImgData rhodat(io, (gsl_matrix *) rho);
+	rhodat.writedata("./zern-test_rhodata.fits", ImgData::FITS);
+
+	gsl_matrix *tmp;
+	
+	for (int m=0; m<zerntest.get_nmodes(); m++) {
+		printf("zern-test.cc:: storing mode %d\n", m);
+		tmp = zerntest.get_mode(m);
+		{
+			ImgData tmpimg(io, (gsl_matrix *) tmp, true);
+			tmpimg.writedata(format("./zern-test_zern_%03d.fits", m), ImgData::FITS, true);
+		}
+	}
+
+	gsl_vector *vec = gsl_vector_calloc(zerntest.get_nmodes());
+	for (int m=0; m<zerntest.get_nmodes(); m++) {
+		printf("zern-test.cc:: testing linear combinations only mode %d\n", m);
+		gsl_vector_set(vec, m, 1.0);
+		if (m>0)
+			gsl_vector_set(vec, m-1, 0);
+		
+		tmp = zerntest.get_modesum(vec);
+		{
+			ImgData tmpimg(io, (gsl_matrix *) tmp, true);
+			tmpimg.writedata(format("./zern-test_zernsum_%03d.fits", m), ImgData::FITS, true);
+		}
+	}
+
+	for (int m=0; m<zerntest.get_nmodes(); m++) {
+		printf("zern-test.cc:: testing linear combinations up to mode %d\n", m);
+		gsl_vector_set(vec, m, 1.0);
+		
+		tmp = zerntest.get_modesum(vec);
+		{
+			ImgData tmpimg(io, (gsl_matrix *) tmp, true);
+			tmpimg.writedata(format("./zern-test_zerncumsum_%03d.fits", m), ImgData::FITS, true);
+		}
+	}
+	
+	for (int m=0; m<zerntest.get_nmodes(); m++)
+		gsl_vector_set(vec, m, drand48());
+
+	printf("zern-test.cc:: testing random vector\n");
+	tmp = zerntest.get_modesum(vec);
+	{
+		ImgData tmpimg(io, (gsl_matrix *) tmp, true);
+		tmpimg.writedata("./zern-test_zern_random.fits", ImgData::FITS, true);
+	}
 	
 	return 0;
 }
