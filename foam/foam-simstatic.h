b6b355aab86df6cffb541b9e0e7ffadbf9106fd7
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-07-25 14:10:23 +0200

    Improved foam-simstatic documentation

--- foam/foam-simstatic.h
+++ foam/foam-simstatic.h
@@ -72,3 +72,49 @@ public:
 
 #endif // HAVE_FOAM_SIMSTATIC_H
 
+/*! \page ud_foamss FOAM static-simulation
+ 
+ \section fss_aboutdoc About this document
+ 
+ This is the (user) documentation for FOAM static-simulation, a simulation 
+ module included with FOAM. This describes how to use and test it, not how
+ to extend it yourself.
+ 
+ \section fss_aboutfoamss About FOAM static-simulation
+ 
+ This module is capable of operating like a 'real' AO system without hardware.
+ It uses a static frame as input from the camera, and does not drive any
+ electronics such as a deformable mirror or tip-tilt mirror. The computational
+ load is the same, however, such that 
+ 
+ \section fss_simu Simulation procedure
+ 
+ Static simulation is performed as follows:
+ 
+ - A simulated camera frame is loaded from disk. This image is used as 'camera'
+ - The image is processed as if it were real, resulting in a wavefront
+ - The wavefront information is converted into a set of actuation commands, 
+    which are not used 
+ 
+ \section fss_simu Usage
+ 
+ To use this module, first start it up like:
+ 
+ - ./foam/foam-simstat -c conf/foam-simstat2.cfg
+ 
+ and connect to it (preferably with the foam-gui):
+ 
+ - ./ui/foam-gui
+ - connect to 'localhost' port '1025' (default)
+ 
+ From the GUI you can run in 'Listen', 'Open loop' or 'Closed loop' mode.
+ 
+ - Listen does nothing and waits for commands
+ - Open loop calculates image shifts but nothing else
+ - Closed loop calculates image shifts and then calculates what commands to 
+ send to a hypothetical deformable mirror
+ 
+ For more information on the devices that static simulation uses, see the 
+ \ref dev_cam_imgcam and \ref dev_wfs_shwfs documentation.
+
+ */
