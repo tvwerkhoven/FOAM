e3bb8e99c836bd68ef190ba21a0a329a78028968
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-08-30 16:56:04 +0200

    Updated/simplified conigure process
    Update configure.ac, use more autoconf internals, compact code, unify naming scheme
    Update makefiles accordingly

--- common.mk
+++ common.mk
@@ -8,16 +8,20 @@ MODS_DIR = $(top_builddir)/mods
 LIB_DIR = $(top_builddir)/lib
 LIBSIU_DIR = $(top_srcdir)/$(LIBSIU)
 
+### Common flags
+
 AM_CPPFLAGS = \
         -I$(LIB_DIR) \
         -I$(MODS_DIR) \
         -I$(LIBSIU_DIR) \
 				$(COMMON_CFLAGS)
 
-### Common flags
 AM_CPPFLAGS += -D__STDC_FORMAT_MACROS \
 		-D__STDC_LIMIT_MACROS \
 		-DFOAM_DATADIR=\"$(datadir)/foam\"
+
+# More error reporting during compilation
+AM_CXXFLAGS = -Wall -Wextra -Wfatal-errors
 
 ### GSL flags
 AM_CPPFLAGS += -DHAVE_INLINE 
@@ -26,19 +30,22 @@ if !HAVE_DEBUG
 AM_CPPFLAGS += -DGSL_RANGE_CHECK_OFF
 endif !HAVE_DEBUG
 
-### More error reporting during compilation
-AM_CXXFLAGS = -Wall -Wextra -Wfatal-errors
-
-### Debug flags
-if HAVE_DEBUG
+### Debug options (first check strict debug, then regular. Add flags accordingly)
+if HAVE_STR_DEBUG
 AM_CPPFLAGS += -D_GLIBCXX_DEBUG \
 		-D_GLIBCXX_DEBUG_PEDANTIC \
 		-D_GLIBCXX_FULLY_DYNAMIC_STRING \
-		-DGLIBCXX_FORCE_NEW \
-		-DFOAM_DEBUG=1
-AM_CXXFLAGS += -O0 -ggdb -g3 -fno-inline
+		-DGLIBCXX_FORCE_NEW
+endif
+
+if HAVE_DEBUG
+AM_CXXFLAGS += -ggdb -g3 -O0 -fno-inline
 else
 AM_CXXFLAGS += -O3 -ftree-vectorize
 endif
-		
+
+### Profiling options
+if HAVE_PROFILING
+AM_CXXFLAGS += -pg
+endif HAVE_PROFILING
 	
