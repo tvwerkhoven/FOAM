b0d90b0dd5a7957747d8938a241666a9e0807179
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-09-07 13:47:22 +0200

    Added BarGraph widget
    BarGraph widget can be used to display bargraphs. It also shows the min and
    maximum values and a list with all the values so the user can copy them.
    Furthermore there is a slot_update() function slot that can hold a pointer
    to the function that can trigger a request for updated values. This slot
    can be called using the refresh button or at regular intervals with the
    auto-update button.

--- ui/widgets.cc
+++ ui/widgets.cc
@@ -17,10 +17,168 @@
  You should have received a copy of the GNU General Public License
  along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <sys/time.h>
+
 #include "widgets.h"
+#include "utils.h"				// For clamp(x, min, max)
 
 using namespace std;
 using namespace Gtk;
+
+BarGraph::BarGraph(const int width, const int height):
+e_minval("Min"), e_maxval("Max"), e_allval("All"),
+b_refresh(Gtk::Stock::REFRESH),
+b_autoupd("Auto Update"), e_autointval("", "Sec"),
+gr_align(0.5, 0.5, 0.0, 0.0) {
+	// Set entry fields to non editable (read-only)
+	e_minval.set_width_chars(6);
+	e_minval.set_editable(false);
+	e_maxval.set_width_chars(6);
+	e_maxval.set_editable(false);
+	e_allval.set_width_chars(14);
+	e_allval.set_editable(false);
+	
+	e_autointval.set_width_chars(6);
+	e_autointval.set_text("1");
+	
+	// Initialize pixbuf and image
+	gr_pixbuf = Gdk::Pixbuf::create(Gdk::COLORSPACE_RGB, false, 8, width, height);
+	gr_pixbuf->fill(0xFFFFFF00);
+	gr_img.set(gr_pixbuf);
+	gr_img.set_double_buffered(false);
+	
+	// Pack boxes
+	hbox0.pack_start(e_minval, PACK_SHRINK);
+	hbox0.pack_start(e_maxval, PACK_SHRINK);
+
+	vbox1.pack_start(hbox0, PACK_SHRINK);
+	vbox1.pack_start(e_allval, PACK_SHRINK);
+	vbox1.pack_start(hsep1, PACK_SHRINK);
+	vbox1.pack_start(b_refresh, PACK_SHRINK);
+
+	hbox1.pack_start(b_autoupd, PACK_SHRINK);
+	hbox1.pack_start(e_autointval, PACK_SHRINK);
+
+	vbox1.pack_start(hbox1, PACK_SHRINK);
+
+	// Pack & stack boxes in eachother
+	gr_events.add(gr_img);
+	gr_align.add(gr_events);
+	
+	// Add vbox1 and gr_align to *this HBox
+	pack_start(vbox1, PACK_SHRINK);
+	pack_start(gr_align, PACK_SHRINK);
+	
+	// Connect events
+	b_refresh.signal_clicked().connect(sigc::mem_fun(*this, &BarGraph::do_update));
+	b_autoupd.signal_clicked().connect(sigc::mem_fun(*this, &BarGraph::on_autoupd_clicked));
+	
+	Glib::signal_timeout().connect(sigc::mem_fun(*this, &BarGraph::on_timeout), 1000.0/30.0);
+}
+
+BarGraph::~BarGraph() {
+	//! @todo delete Pixbuf here?
+	
+}
+
+bool BarGraph::on_timeout() {
+	// This function fires 30 times/s, if we do not want that many updates we can throttle it with subi_vecdelay
+	static struct timeval now, last;
+	gettimeofday(&now, NULL);
+	// If b_autoupd is 'clear' we don't want shifts, if 'waiting' we're expecting new shifts soon, otherwise if button is 'OK': get new shifts
+	string str_autointval = e_autointval.get_text();
+	if (b_autoupd.get_state() == SwitchButton::OK && 
+			((now.tv_sec - last.tv_sec) + (now.tv_usec - last.tv_usec)/1000000.) > strtof(str_autointval.c_str(), NULL) ) {
+		b_autoupd.set_state(SwitchButton::WAITING);
+		fprintf(stderr, "BarGraph::on_timeout(): slot_update()\n");
+		slot_update();
+		gettimeofday(&last, NULL);
+	}
+	return true;
+}
+
+void BarGraph::on_autoupd_clicked() {
+	// Click the 'b_autoupd' button: 
+	// -> if CLEAR: set to OK and run on_timeout()
+	// -> if WAITING, OK or ERROR: set to CLEAR and stop everything
+	if (b_autoupd.get_state() == SwitchButton::CLEAR) {
+		b_autoupd.set_state(SwitchButton::OK);
+		on_timeout();
+	}
+	else
+		b_autoupd.set_state(SwitchButton::CLEAR);
+}
+
+void BarGraph::on_update(const std::vector< double > &graph_vals) {
+	if (graph_vals.size() <= 0)
+		return;
+	
+	// range is -1 -- 1 for all modes. Check actual range:
+	double min=graph_vals.at(0);
+	double max=min;
+	string allvals = format("%.4f", graph_vals.at(0));
+	for (size_t i=1; i<graph_vals.size(); i++) {
+		if (fabs(graph_vals.at(i)) > max)
+			max = fabs(graph_vals.at(i));
+		else if (fabs(graph_vals.at(i)) < min)
+			min = fabs(graph_vals.at(i));
+		allvals += format(", %.4f", graph_vals.at(i));
+	}
+	
+	e_minval.set_text(format("%.4f", min));
+	e_maxval.set_text(format("%.4f", max));
+	e_allval.set_text(allvals);
+	
+	// make background white
+	gr_pixbuf->fill(0xffffff00);
+	
+	// Draw bars
+	uint8_t *out = (uint8_t *) gr_pixbuf->get_pixels();
+	
+	int nvals = graph_vals.size();
+	int w = gr_pixbuf->get_width();
+	int h = gr_pixbuf->get_height();
+	
+	// The pixbuf is w pixels wide, we have nvals values, so each column is colw wide:
+	int colw = (w/nvals)-1;
+	
+	//! @bug this fails for more than ~200 modes, add scrollbar?
+	if (colw <= 0) {
+		fprintf(stderr, "BarGraph::do_update(): error, too many values, cannot draw!\n");
+		return;
+	}
+	
+	uint8_t col[3];
+	for (int n = 0; n < nvals; n++) {
+		double amp = clamp(graph_vals.at(n), (double)-1.0, (double)1.0);
+		int height = amp*h/2.0; // should be between -h/2 and h/2
+		
+		// Set bar color (red 2%, orange 10% or green rest):
+		if (fabs(amp)>0.98) {
+			col[0] = 255; col[1] = 000; col[2] = 000; // X11 red
+		} else if (fabs(amp)>0.90) {
+			col[0] = 255; col[1] = 165; col[2] = 000; // X11 orange
+		} else {
+			col[0] = 144; col[1] = 238; col[2] = 144; // X11 lightgreen
+		}
+		
+		// Bars going down (first part) or up (second part)
+		if (height < 0) {
+			for (int x = n*colw; x < (n+1)*colw; x++) {
+				for (int y = h/2; y > h/2+height; y--) {
+					uint8_t *p = out + 3 * (x + w * y);
+					p[0] = col[0]; p[1] = col[1]; p[2] = col[2];
+				} } } 
+		else {
+			for (int x = n*colw; x < (n+1)*colw; x++) {
+				for (int y = h/2; y < h/2+height; y++) {
+					uint8_t *p = out + 3 * (x + w * y);
+					p[0] = col[0]; p[1] = col[1]; p[2] = col[2];
+				} } }
+	}
+	gr_img.queue_draw();
+}
 
 
 SwitchButton::SwitchButton(const Glib::ustring &lbl): 
