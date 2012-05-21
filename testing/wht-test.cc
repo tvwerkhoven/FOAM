22ce89b38e4e824d60cccf386cda34e99bbfcf2f
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2012-05-21 14:45:42 +0200

    Added WHT test function

---
+++ testing/wht-test.cc
@@ -0,0 +1,58 @@
+/*
+ wht-test.cc -- test WHT interface
+ Copyright (C) 2012 Tim van Werkhoven <werkhoven@strw.leidenuniv.nl>
+ 
+ This file is part of FOAM.
+ 
+ FOAM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ FOAM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FOAM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+
+#include "serial.h"
+#include "socket.h"
+#include "io.h"
+#include "telescope.h"
+#include "wht.h"
+
+using namespace std;
+
+int main() {
+	printf("Init Io...\n");
+	Io io(10);
+	
+	io.msg(IO_INFO, "Init foamctrl...");
+	foamctrl ptc(io);
+	
+	io.msg(IO_INFO, "Init WHT...");
+	WHT *wht;
+	try {
+		wht = new WHT(io, &ptc, "wht-test", "1234", Path("./wht-test.cfg"), true);
+	} catch (...) {
+		io.msg(IO_ERR, "Failed to initialize WHT, deleting & aborting!");
+		return 1;
+	}
+	sleep(1);
+	
+	io.msg(IO_INFO, "Init complete.");
+
+	
+	io.msg(IO_INFO, "Quitting now...");
+	delete wht;
+	
+	io.msg(IO_INFO, "Program exit in 1 seconds...");
+	sleep(1);
+	
+	return 0;
+}
